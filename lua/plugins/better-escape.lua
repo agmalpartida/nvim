e845ac239a04b99cd8b797cff654b32cd8deba8f
Alberto Galvez <agmalpartida@gmail.com>
2024-06-07 18:30:50 +0200

    Automatic commit: 20240607-18:30

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,12 +1,24 @@
-return function()
-	require("better_escape").setup({
-		mapping = { "jk", "kj" }, -- a table with mappings to use
-		timeout = 500, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-		clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-		keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-		-- example(recommended)
-		-- keys = function()
-		--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-		-- end,
-	})
-end
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize None-ls sources
+
+---@type LazySpec
+
+return {
+  "max397574/better-escape.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    -- local null_ls = require "null-ls"
+
+    -- Check supported formatters and linters
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      mapping = { "jk", "kj" }, -- a table with mappings to use
+      timeout = 500, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+      clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+      --keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+    }
+    return config -- return final config table
+  end,
+}
