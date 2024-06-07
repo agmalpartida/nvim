31272f96c648f17a8c811ee87b7e6e0a0b3d89c9
Alberto Galvez <agmalpartida@gmail.com>
2024-06-07 18:00:49 +0200

    Automatic commit: 20240607-18:00

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -40,14 +40,7 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  --{ "max397574/better-escape.nvim", enabled = false },
-  {
-    "max397574/better-escape.nvim",
-    mapping = { "kj", "jk", "jj" }, -- a table with mappings to use
-    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-  },
+  { "max397574/better-escape.nvim", enabled = true },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
