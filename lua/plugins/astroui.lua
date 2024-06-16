a77d33aa329ddf5646d843793b88e7b8760d094f
Alberto Galvez <agmalpartida@gmail.com>
2024-06-16 12:50:52 +0200

    Automatic commit: 20240616-12:50

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -12,7 +12,6 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    -- colorscheme = "solarized-osaka", 
     colorscheme = "kanagawa",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
