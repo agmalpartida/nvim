253fe7c5540a5db82562b1482c811d45df0351be
Alberto Galvez <alg@fullstep.com>
2024-07-02 19:40:58 +0200

    Automatic commit: 20240702-19:40

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -35,7 +35,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 3200, -- default format timeout
+      --timeout_ms = 3200, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -84,6 +84,7 @@ return {
     capabilities = {
       textDocument = {
         foldingRange = { dynamicRegistration = false },
+        lineFoldingOnly = true,
       },
     },
     -- mappings to be set up on attaching of a language server
