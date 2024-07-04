49882f917a79c8a31a3e3578d43c41c5cf08dfd6
Alberto Galvez <agmalpartida@gmail.com>
2024-07-04 21:00:53 +0200

    Automatic commit: 20240704-21:00

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   -- import/override with your plugins folder
 }
