b798fcfbea7689b7f007a10e6af252a8b78f0355
Alberto Galvez <agmalpartida@gmail.com>
2024-07-04 23:40:50 +0200

    Automatic commit: 20240704-23:40

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
