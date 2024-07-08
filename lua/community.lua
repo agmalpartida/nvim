c0b5538f79c15580380455b026371bf7fd74a006
Alberto Galvez <agmalpartida@gmail.com>
2024-07-08 21:00:52 +0200

    Automatic commit: 20240708-21:00

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   -- import/override with your plugins folder
 }
