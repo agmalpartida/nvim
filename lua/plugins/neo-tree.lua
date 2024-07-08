fb67233061f36df619e35591a14f47e1d72d956c
Alberto Galvez <agmalpartida@gmail.com>
2024-07-08 22:30:52 +0200

    Automatic commit: 20240708-22:30

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+      filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false, 
+        hide_gitignored = false,
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db",
+          "node_modules",
+          "__pycache__",
+        },
+      },
+  }
+}
