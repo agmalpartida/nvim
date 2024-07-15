78a502254f931735967ecc9bca34fd1929367937
Alberto Galvez <agmalpartida@gmail.com>
2024-07-15 22:30:53 +0200

    Automatic commit: 20240715-22:30

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -64,7 +64,7 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  --{ "max397574/better-escape.nvim", enabled = false },
   -- {
   --   "max397574/better-escape.nvim",
   --   opts = function(_, opts)
