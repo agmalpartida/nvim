ca44d48851b130d50f238ecdae1de0c873e8c002
Alberto Galvez <agmalpartida@gmail.com>
2024-07-20 19:20:50 +0200

    Automatic commit: 20240720-19:20

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
+--- we do have the binding <leader>fF which searches through all files as as the <leader>ff which only searches non-hidden files
 ---@type LazySpec
 return {
-  plugins = {
-    telescope = {
-      pickers = {
-        find_files = {
-          hidden = true,
+ "goolord/alpha-nvim",
+    plugins = {
+      telescope = {
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
         },
       },
     },
-  },
 }
