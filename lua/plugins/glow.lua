5f43273592fd34400c233088eb7128e49a17acd1
Alberto Galvez <agmalpartida@gmail.com>
2024-07-20 21:35:46 +0200

    wip

--- lua/plugins/glow.lua
+++ lua/plugins/glow.lua
@@ -2,7 +2,7 @@
 return {
   "ellisonleao/glow.nvim",
   opts = {
-    glow_path = "/usr/local/bin/glow", -- will be filled automatically with your glow bin in $PATH, if any
+    --glow_path = "/usr/local/bin/glow", -- will be filled automatically with your glow bin in $PATH, if any
     install_path = "~/.local/bin", -- default path for installing glow binary
     border = "shadow", -- floating window border config
     style = "dark", -- dark|light, filled automatically with your current editor background, you can override using glow json style
