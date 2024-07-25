35735347cd5ec1b958f4aff2f78bd22b80052490
Alberto Galvez <agmalpartida@gmail.com>
2024-07-25 18:30:54 +0200

    Automatic commit: 20240725-18:30

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
     -- change colorscheme
     --colorscheme = "astrodark",
     colorscheme = "astromars",
-    -- colorscheme = "astrolight",
+    --colorscheme = "astrolight",
     -- colorscheme = "onedark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     -- highlights = {
