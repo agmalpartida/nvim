d6cfe62d17c1164755ccafddff26bd0abf3ba011
Alberto Galvez <agmalpartida@gmail.com>
2024-07-29 23:20:50 +0200

    Automatic commit: 20240729-23:20

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000, -- make sure to load this before all the other start plugins
+  -- Optional; default configuration will be used if setup isn't called.
+  config = function()
+    require("everforest").setup({
+      on_highlights = function(hl, palette)
+      hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
+      hl.DiagnosticWarn = { fg = palette.none, bg = palette.none, sp = palette.yellow }
+      hl.DiagnosticInfo = { fg = palette.none, bg = palette.none, sp = palette.blue }
+      hl.DiagnosticHint = { fg = palette.none, bg = palette.none, sp = palette.green }
+  end,
+    })
+  end,
+}
