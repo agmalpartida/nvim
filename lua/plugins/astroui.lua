cffdb760c48b30977195c32af5b3b93ac7cea2a8
Alberto Galvez <agmalpartida@gmail.com>
2024-07-29 23:30:49 +0200

    Automatic commit: 20240729-23:30

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -8,22 +8,37 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
-    --colorscheme = "onedark",
-    colorscheme = "everforest",
-    -- Icons can be configured throughout the interface
-    icons = {
-      -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
-    },
+  -- Colorscheme set on startup, a string that is used with `:colorscheme astrodark`
+    colorscheme = "astrodark",
+  -- Override highlights in any colorscheme
+  -- Keys can be:
+  --   `init`: table of highlights to apply to all colorschemes
+  --   `<colorscheme_name>` override highlights in the colorscheme with name: `<colorscheme_name>`
+    -- highlights = {
+    -- -- this table overrides highlights in all colorschemes
+    -- init = {
+    --   Normal = { bg = "#000000" },
+    -- },
+    -- -- a table of overrides/changes when applying astrotheme
+    -- astrotheme = {
+    --   Normal = { bg = "#000000" },
+    -- },
+    -- },
+    -- -- change colorscheme
+    -- colorscheme = "astrodark",
+    -- -- Icons can be configured throughout the interface
+    -- icons = {
+    --   -- configure the loading of the lsp in the status line
+    --   LSPLoading1 = "⠋",
+    --   LSPLoading2 = "⠙",
+    --   LSPLoading3 = "⠹",
+    --   LSPLoading4 = "⠸",
+    --   LSPLoading5 = "⠼",
+    --   LSPLoading6 = "⠴",
+    --   LSPLoading7 = "⠦",
+    --   LSPLoading8 = "⠧",
+    --   LSPLoading9 = "⠇",
+    --   LSPLoading10 = "⠏",
+    -- },
   },
 }
