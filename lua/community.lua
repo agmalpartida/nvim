d62d4e71294df13695f92237960f631e5847b93a
Alberto Galvez <agmalpartida@gmail.com>
2024-08-04 00:00:51 +0200

    Automatic commit: 20240804-00:00

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.code-runner.sniprun" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   -- import/override with your plugins folder
---
+++ lua/plugins/sniprun.lua
@@ -0,0 +1,7 @@
+---@type LazySpec
+return {
+"michaelb/sniprun",
+  opts = {
+    display = { "Terminal" },
+  },
+}
