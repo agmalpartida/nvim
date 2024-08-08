c2e6e39cf2f23c27c7bc23771504ca838821365e
Alberto Galvez <agmalpartida@gmail.com>
2024-08-08 21:50:54 +0200

    Automatic commit: 20240808-21:50

--- lua/plugins/glow.lua
+++ lua/plugins/glow.lua
@@ -1,14 +1,14 @@
---@type LazySpec
+---@type LazySpec
 return {
-"ellisonleao/glow.nvim",
+  "ellisonleao/glow.nvim",
   opts = {
     --glow_path = "/usr/local/bin/glow", -- will be filled automatically with your glow bin in $PATH, if any
     --install_path = "~/.local/bin", -- default path for installing glow binary
     --border = "shadow", -- floating window border config
-    style = "light", -- dark|light, filled automatically with your current editor background, you can override using glow json style
+    style = "dark", -- dark|light, filled automatically with your current editor background, you can override using glow json style
     pager = false,
-    -- width = 1280,
-    -- height = 100,
+    width = 100,
+    height = 100,
     width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
     height_ratio = 0.7,                            -- that has to pass before starting new render
   },
