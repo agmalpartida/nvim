c713116672404702434563c4c33de88d26d217c2
Alberto Galvez <agmalpartida@gmail.com>
2024-08-12 14:10:55 +0200

    Automatic commit: 20240812-14:10

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,6 @@ return {
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   -- import/override with your plugins folder
 }
--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax", "kanagawa" } },
+  install = { colorscheme = { "astrodark", "habamax", "kanagawa", "everforest" } },
   ui = { backdrop = 100 },
   plugins = {
     better_escape = {
