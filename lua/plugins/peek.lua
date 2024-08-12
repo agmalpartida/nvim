d4f1968de730b02346856f2cade3c049929a340c
Alberto Galvez <agmalpartida@gmail.com>
2024-08-12 14:30:58 +0200

    Automatic commit: 20240812-14:30

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -1,6 +1,8 @@
 ---@type LazySpec
 return {
     "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
     config = function()
         require("peek").setup()
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
@@ -12,12 +14,10 @@ return {
     syntax = true,            -- enable syntax highlighting, affects performance
     theme = 'dark',           -- 'dark' or 'light'
     update_on_change = true,
-    app = 'webview',          -- 'webview', 'browser', string or a table of strings
-                              -- explained below
+    app = 'chromium',          -- 'webview', 'browser', string or a table of strings
     filetype = { 'markdown' },-- list of filetypes to recognize as markdown
     -- relevant if update_on_change is true
     throttle_at = 200000,     -- start throttling when file exceeds this
                               -- amount of bytes in size
     throttle_time = 'auto',   -- minimum amount of time in milliseconds
-                              -- that has to pass before starting new render
 }
