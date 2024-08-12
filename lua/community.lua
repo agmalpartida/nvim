66b4799a53fa65941ba5d777c259213f9c897eb5
Alberto Galvez <agmalpartida@gmail.com>
2024-08-12 18:10:52 +0200

    Automatic commit: 20240812-18:10

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   -- import/override with your plugins folder
 }
