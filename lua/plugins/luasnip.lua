b40d9968f3cc5a84f3a0172636d28afef4990d4f
Alberto Galvez <agmalpartida@gmail.com>
2024-08-22 21:30:52 +0200

    Automatic commit: 20240822-21:30

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -5,6 +5,16 @@ return {
       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
+      vim.keymap.set({ "i", "s" }, "<C-l>", function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end)
+      vim.keymap.set({ "i", "s" }, "<C-h>", function()
+        if luasnip.choice_active() then
+       luasnip.change_choice(-1)
+       end
+      end)
       luasnip.filetype_extend("javascript", { "javascriptreact" })
       -- load snippets paths
       require("luasnip.loaders.from_vscode").lazy_load {
