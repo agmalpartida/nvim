c67fb8fe4c7c5bf888267fd55904a338176e0138
Alberto Galvez <agmalpartida@gmail.com>
2024-08-23 20:10:51 +0200

    Automatic commit: 20240823-20:10

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -4,6 +4,18 @@
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
 
+-- function for calculating the current session name
+local get_session_name = function()
+  local name = vim.fn.getcwd()
+  local branch = vim.fn.system("git branch --show-current")
+  if vim.v.shell_error == 0 then
+    return name .. vim.trim(branch --[[@as string]])
+  else
+    return name
+  end
+end
+
+
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
@@ -12,6 +24,37 @@ return {
     autocmds = {
       -- disable alpha autostart
       -- alpha_autostart = false,
+      git_branch_sessions = {
+        -- auto save directory sessions on leaving
+        {
+          event = "VimLeavePre",
+          desc = "Save git branch directory sessions on close",
+          callback = vim.schedule_wrap(function()
+            if require("astrocore.buffer").is_valid_session() then
+              require("resession").save(
+                get_session_name(),
+                { dir = "dirsession", notify = false }
+              )
+            end
+          end),
+        },
+        -- auto restore previous previous directory session, remove if necessary
+        {
+          event = "VimEnter",
+          desc = "Restore previous directory session if neovim opened with no arguments",
+          nested = true, -- trigger other autocommands as buffers open
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+              -- try to load a directory session using the current working directory
+              require("resession").load(
+                get_session_name(),
+                { dir = "dirsession", silence_errors = true }
+              )
+            end
+          end,
+        },
+      },
       restore_session = {
         {
           event = "VimEnter",
@@ -82,6 +125,53 @@ return {
       autosave = {
         last = true, -- auto save last session
         cwd = true, -- auto save session for each working directory
+      },
+      mappings = {
+        n = {
+          -- update save dirsession mapping to get the correct session name
+          ["<Leader>SS"] = {
+            function()
+              require("resession").save(
+                get_session_name(),
+                { dir = "dirsession" }
+              )
+            end,
+            desc = "Save this dirsession",
+          },
+          -- update load dirsession mapping to get the correct session name
+          ["<Leader>S."] = {
+            function()
+              require("resession").load(
+                get_session_name(),
+                { dir = "dirsession" }
+              )
+            end,
+            desc = "Load current dirsession",
+          },
+        },
+
+      },
+      n = {
+        -- update save dirsession mapping to get the correct session name
+        ["<Leader>SS"] = {
+          function()
+            require("resession").save(
+              get_session_name(),
+              { dir = "dirsession" }
+            )
+          end,
+          desc = "Save this dirsession",
+        },
+        -- update load dirsession mapping to get the correct session name
+        ["<Leader>S."] = {
+          function()
+            require("resession").load(
+              get_session_name(),
+              { dir = "dirsession" }
+            )
+          end,
+          desc = "Load current dirsession",
+        },
       },
       -- Patterns to ignore when saving sessions
       ignore = {
