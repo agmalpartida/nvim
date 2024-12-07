1626839b9c28fe68d0eff8c98f1fc0f03ab12c7e
Alberto Galvez <agmalpartida@gmail.com>
2024-12-07 20:21:50 +0100

    Automatic commit: 20241207-20:20

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,34 +8,34 @@ return {
         container = {
           enable_character_fade = false,
         },
-      file_size = {
-        enabled = true,
-        required_width = 24, -- min width of window required to show this column
+        -- file_size = {
+        --   enabled = true,
+        --   required_width = 24, -- min width of window required to show this column
+        -- },
+        -- type = {
+        --   enabled = true,
+        --   required_width = 122, -- min width of window required to show this column
+        -- },
+        -- last_modified = {
+        --   enabled = true,
+        --   required_width = 88, -- min width of window required to show this column
+        -- },
+        -- created = {
+        --   enabled = true,
+        --   required_width = 110, -- min width of window required to show this column
+        -- },
+        -- symlink_target = {
+        --   enabled = false,
+        -- },
       },
-      type = {
-        enabled = true,
-        required_width = 122, -- min width of window required to show this column
-      },
-      last_modified = {
-        enabled = true,
-        required_width = 88, -- min width of window required to show this column
-      },
-      created = {
-        enabled = true,
-        required_width = 110, -- min width of window required to show this column
-      },
-      symlink_target = {
-        enabled = false,
-      },
-      },
-              window = {
+      window = {
           position = "left",
           width = 40,
           mapping_options = {
             noremap = true,
             nowait = true,
           },
-          },
+      },
       filesystem = {
         follow_current_file = {
           enabled = true,
