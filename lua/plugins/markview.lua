2b3315fc7e7c8e214d35e15a5890394da9e13103
Alberto Galvez <me@albertogalvez.com>
2025-02-04 22:11:51 +0100

    Automatic commit: 20250204-22:10

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,5 +1,16 @@
 -- For `plugins/markview.lua` users.
 return {
     "OXY2DEV/markview.nvim",
-    lazy = false
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+      dir_path = "assets", ---@type string | fun(): string
+      extension = "png", ---@type string | fun(): string
+      file_name = "%Y-%m-%d-%H-%M-%S", ---@type string | fun(): string
+      use_absolute_path = true,
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
 };
