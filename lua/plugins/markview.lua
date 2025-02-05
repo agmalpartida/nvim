1e40fc47d3bc1afd9e897d46d7da0d954ed08a44
Alberto Galvez <me@albertogalvez.com>
2025-02-05 16:11:58 +0100

    Automatic commit: 20250205-16:10

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,14 +1,27 @@
 -- For `plugins/markview.lua` users.
 return {
   "OXY2DEV/markview.nvim",
-  opts = function(_, opts)
-    opts.config = {
-      preview = {
-          enable = true,
-      },
-      markdown = {
-          enable = false,
-      },
-    }
-  end,
+   opts = function(_, opts)
+     -- opts.experimental = {
+     --   date_formats = {},
+     --   date_time_formats = {},
+     --   text_filetypes = {},
+     --   read_chunk_size = 1000,
+     --   link_open_alerts = false,
+     --   file_open_command = "tabnew",
+     --   list_empty_line_tolerance = 3,
+     -- }
+     opts.highlight_groups = {}
+     opts.preview = {
+       enable = true, -- Habilita la vista previa
+       filetypes = { "md", "rmd", "quarto" }, -- Tipos de archivos soportados
+       ignore_buftypes = { "nofile" },
+       ignore_previews = {},
+     }
+     opts.auto_start = true -- Iniciar automáticamente la vista previa al abrir un Markdown
+     opts.auto_close = true -- Cerrar la vista previa al salir de Neovim
+     opts.open_cmd = "firefox" -- Cambia por "google-chrome", "brave", etc.
+     opts.port = 8000 -- Puerto para la vista previa
+     opts.theme = "dark" -- Opciones: "light", "dark", "system"
+   end,
 };
