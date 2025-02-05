34aa3e916b719cf33a07e2b1589789e3732ba8a3
Alberto Galvez <me@albertogalvez.com>
2025-02-05 14:21:56 +0100

    Automatic commit: 20250205-14:20

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -1,19 +1,14 @@
 ---@type LazySpec
 return {
-    "HakonHarnes/img-clip.nvim",
-    opts = {
-      opts = {
-        -- add options here
-        -- or leave it empty to use the default settings
-        dir_path = "TEST",
-        extension = "png",
-        file_name = "%Y-%m-%d-%H-%M-%S",
-        relative_to_current_file = true,
-      },
-      keys = {
-        -- suggested keymap
-        { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-      },
-    },
+  "HakonHarnes/img-clip.nvim",
+  opts = function(_, opts)
+    opts.default = {
+     dir_path = "assets", 
+     extension = "png",
+     file_name = "%Y-%m-%d-%H-%M-%S",
+     use_absolute_path = false,
+     relative_to_current_file = true,
+    }
+  end,
 }
 
