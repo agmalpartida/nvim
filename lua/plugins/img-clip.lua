79364598c04a508d5449c300b0cffda3dd5a3cf4
Alberto Galvez <me@albertogalvez.com>
2025-02-05 14:11:58 +0100

    Automatic commit: 20250205-14:10

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -2,16 +2,18 @@
 return {
     "HakonHarnes/img-clip.nvim",
     opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
-      dir_path = "TEST",
-      extension = "png",
-      file_name = "%Y-%m-%d-%H-%M-%S",
-      relative_to_current_file = true,
-    },
-    keys = {
-      -- suggested keymap
-      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+      opts = {
+        -- add options here
+        -- or leave it empty to use the default settings
+        dir_path = "TEST",
+        extension = "png",
+        file_name = "%Y-%m-%d-%H-%M-%S",
+        relative_to_current_file = true,
+      },
+      keys = {
+        -- suggested keymap
+        { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+      },
     },
 }
 
