6c6203e6fc551a2d4af43544d555c45e25ffcac5
Alberto Galvez <me@albertogalvez.com>
2025-02-05 14:41:58 +0100

    Automatic commit: 20250205-14:40

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -4,14 +4,16 @@ return {
   opts = function(_, opts)
     opts.default = {
       -- file and directory options
-      -- dir_path = "assets",
-      dir_path = function()
-        return vim.fn.expand("%:t:r")
-      end,
+      dir_path = "assets",
+      -- dir_path = function()
+      --   return vim.fn.expand("%:t:r")
+      -- end,
       extension = "png",
       file_name = "%Y-%m-%d-%H-%M-%S",
       use_absolute_path = false,
       relative_to_current_file = true,
+      -- template options
+      template = "$FILE_PATH",
       -- prompt options
       prompt_for_file_name = false,
     }
