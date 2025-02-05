f11c2a5d0b5c2ec62aa6af18628bf9a9bf808151
Alberto Galvez <me@albertogalvez.com>
2025-02-05 14:32:00 +0100

    Automatic commit: 20250205-14:30

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -3,11 +3,17 @@ return {
   "HakonHarnes/img-clip.nvim",
   opts = function(_, opts)
     opts.default = {
-     dir_path = "assets", 
-     extension = "png",
-     file_name = "%Y-%m-%d-%H-%M-%S",
-     use_absolute_path = false,
-     relative_to_current_file = true,
+      -- file and directory options
+      -- dir_path = "assets",
+      dir_path = function()
+        return vim.fn.expand("%:t:r")
+      end,
+      extension = "png",
+      file_name = "%Y-%m-%d-%H-%M-%S",
+      use_absolute_path = false,
+      relative_to_current_file = true,
+      -- prompt options
+      prompt_for_file_name = false,
     }
   end,
 }
